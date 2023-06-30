0d6d39cdffad56d7d1a03b86bc458bd7578429e4
Max-Narracott <maxnarracott@gmail.com>
2023-06-30 21:30:35 +0100

    added auth routes and user specific content

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,16 @@
+import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext"
+
 const { Link } = require("react-router-dom")
 
 
 const Navbar = () => {
+    const { logout } = useLogout()
+    const { user } = useAuthContext()
+
+    const handleClick = () => {
+        logout()
+    }
 
 
     return (
@@ -10,6 +19,20 @@ const Navbar = () => {
                 <Link to='/'>
                     <h1>Workout Buddy</h1>
                 </Link>
+                <nav>
+                    {user && ( 
+                    <div>
+                        <span>{user.email}</span>
+                        <button onClick={handleClick}>Log Out</button>
+                    </div>
+                    )}
+                    {!user && (<div>
+                        <Link to='/login'>Log In</Link>
+                        <Link to='/signup'>Sign Up</Link>
+                    </div>
+                    )}
+
+                </nav>
             </div>
         </header>
     )
